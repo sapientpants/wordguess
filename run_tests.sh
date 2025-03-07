b4fea34fc89a9534eed39bd0dce6ad86b01a8941
Marc Tremblay <marc.tremblay@gmail.com>
2025-03-07 10:10:20 +0100

    Remove test service from docker-compose.yml and update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,9 +4,9 @@
 echo "Stopping any running containers..."
 docker compose down
 
-# Build and run the test service
+# Build and run tests in the web container
 echo "Building and running tests..."
-docker compose run --rm test
+docker compose run --rm -e MIX_ENV=test web sh -c "mix deps.get && mix ecto.create && mix ecto.migrate && mix test"
 
 # Clean up
 echo "Cleaning up..."
